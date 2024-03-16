a7909dc224de638379171905f29241feac304811
marcelinoMSR <marcelino-sanchez2011@hotmail.com>
2024-03-16 01:59:23 -0600

    agregamos cosas

---
+++ qp−intpoint.py
@@ -0,0 +1,20 @@
+def qp_intpoint(Q, F, c, d)
+    # x soluci ́on  ́optima del problema.
+    # Min (1/2) ∗ xT Qx + cT x
+    # Sujeto a F x ≥ d
+    #
+    # La tolerancia para las condiciones necesarias de primer orden es
+    # tol = 1e − 05.
+    # N ́umero m ́aximo de iteraciones, maxiter = 100.
+    tol= 1e-05
+    maxiter= 100
+    
+    
+    
+
+
+    return x, μ, iter
+    # x vector soluci ́on del problema
+    # μ multiplicador de Lagrange
+    # iter n ́umero de iteraciones
+    5
